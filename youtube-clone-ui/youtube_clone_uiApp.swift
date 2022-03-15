69b54ac3e3d508d70ab4f0a4b1430684fe3ef14e
irfanizudin <irfanizzudyn@gmail.com>
2022-03-15 21:31:33 +0700

    Initial Commit

---
+++ youtube-clone-ui/youtube_clone_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  youtube_clone_uiApp.swift
+//  youtube-clone-ui
+//
+//  Created by Irfan Izudin on 15/03/22.
+//
+
+import SwiftUI
+
+@main
+struct youtube_clone_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
